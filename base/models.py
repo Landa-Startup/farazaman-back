c26d9a98ed85fc1c5ba062a51989ca070d84d2dd
farazzkamali <farazzkamali80@gmail.com>
2023-05-17 04:36:52 -0400

    feat: create Startup entity

--- base/models.py
+++ base/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Startup (models.Model):
+    name = models.CharField(max_length=255)
+    members = models.TextField()
+    fund = models.CharField(blank=True, null=True, max_length=1000)
+    description = models.TextField(blank=True, null=True)
+    logo = models.CharField(max_length=1255)
+
+    def __str__(self):
+        return self.name
+    
