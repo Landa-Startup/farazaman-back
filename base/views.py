4958d9a1d3c50cf2d1a58108b4adfe9825fc25fd
farazzkamali <farazzkamali80@gmail.com>
2023-05-21 08:25:47 -0400

    feat: StartupSubmitViewSet

--- base/views.py
+++ base/views.py
@@ -1,36 +1,33 @@
 from django.shortcuts import render, HttpResponse
-from .models import Startup
+from .models import Startup, Contact, StartupSubmit
 from rest_framework.decorators import api_view, APIView
 from rest_framework.response import Response
-from rest_framework import status
-from .serializers import StartupSerializer, StartupFormSerializer
-from .forms import StartupForm
+from rest_framework import status, viewsets
+from .serializers import StartupSerializer, ContactSerializer
 
-@api_view(['GET', 'POST'])
+
+
 def index(request):
+    return HttpResponse("index page")
+
+@api_view(['GET'])
+def startups(request):
     if request.method == 'GET':
         startup = Startup.objects.all()
         serializer = StartupSerializer(startup, many=True,context = {startup:startup})
         return Response(serializer.data)
-    if request.method == 'POST':
-        serializer = StartupSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    
+def startupsDetail(request, pk):
+    startup = Startup.objects.get(id=pk)
+    serializer = StartupSerializer(startup, many=False)
+    return Response(serializer.data, pk = startup.id)
 
-   
+    
+class ContactViewSet(viewsets.ModelViewSet):
+    queryset = Contact.objects.all()
+    serializer_class = ContactSerializer
 
 
-# class StartupFormView(APIView):
-#     def post(self, request):
-#         serializer = StartupFormSerializer(data=request.data)
-#         if serializer.is_valid():
-#             # Perform actions based on the form data
-#             name_value = serializer.validated_data['name']
-#             members_value = serializer.validated_data['members']
-#             found_value = serializer.validated_data['found']
-#             #pitch_file = serializer.validated_data['pitch']
-#             # ...
-#             return Response({'success': True})
-#         else:
-#             return Response(serializer.errors, status=400)
+class StartupSubmitViewSet(viewsets.ModelViewSet):
+    queryset = StartupSubmit.objects.all()
+    serializer_class = StartupSerializer
