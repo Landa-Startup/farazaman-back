4e32b110c2073efa1e63f339ad2d96ffa3deef3a
farazzkamali <farazzkamali80@gmail.com>
2023-05-21 09:32:34 -0400

    feat: add routers

--- base/urls.py
+++ base/urls.py
@@ -1,17 +1,33 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import index, ContactViewSet, StartupSubmitViewSet, startups, startupsDetail
+from .views import index, ContactViewSet, StartupSubmitViewSet, StartupViewSet
 from rest_framework import routers
+#import as_view 
 
 
 
 router = routers.DefaultRouter()
-router.register(r'contact', ContactViewSet)
-router.register(r'startupSubmit', StartupSubmitViewSet)
+#include all urls from router
 
-urlpatterns = [
-    path('', index, name="index"),
-    path('startups/', startups, name="startups"),
-    path('startups/<str:pk>', startupsDetail, name="startups"),
-    path('form/', include(router.urls))
-]
+
+#router.register(r'startupSubmit', StartupSubmitViewSet)
+#router.register(r'startups', StartupViewSet)
+
+contact_router = routers.DefaultRouter()
+contact_router.register(r'contact', ContactViewSet)
+
+startups_router = routers.DefaultRouter()
+startups_router.register(r'startups', StartupViewSet)
+startups_router.register(r'startup-submit', StartupSubmitViewSet)
+
+
+
+
+urlpatterns = router.urls + startups_router.urls + contact_router.urls
+
+# urlpatterns = [
+#     path('', index, name="index"),
+#     #path('startups/', startups, name="startups"),
+#     path('startups/', include(router.urls)),
+#     path('form/', include(router.urls))
+# ]
