dfa206d28cbffc8c54b277715a4f1f06fdac4966
farazzkamali <farazzkamali80@gmail.com>
2023-05-21 08:23:00 -0400

    feat: register StartupSubmit

--- base/admin.py
+++ base/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Startup
+from .models import Startup, Contact, StartupSubmit
 
 admin.site.register(Startup)
+admin.site.register(Contact)
+admin.site.register(StartupSubmit)
