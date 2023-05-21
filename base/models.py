086dcbd422adc4b58bb66cdc84d5991ee64d31c0
farazzkamali <farazzkamali80@gmail.com>
2023-05-21 08:23:32 -0400

    feat: create StartupSubmit

--- base/models.py
+++ base/models.py
@@ -11,3 +11,24 @@ class Startup (models.Model):
     def __str__(self):
         return self.name
     
+class Contact(models.Model):
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
+    message = models.TextField()
+    phone = models.CharField(max_length=255, blank=False, null=False, default='+98')
+    created_at =  models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+    
+class StartupSubmit(models.Model):
+    name = models.CharField(max_length=255)
+    members_count = models.IntegerField()
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=255, blank=False, null=False)
+    pitch = models.FileField(upload_to='base/pitches')
+
+    def __str__(self):
+        return self.name
+
+    #get id startup 
