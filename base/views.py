e1516853048d6edbccdec9bb6569fd0a84bdabe2
farazzkamali <farazzkamali80@gmail.com>
2023-05-21 09:33:15 -0400

    feat: add viewSets

--- base/views.py
+++ base/views.py
@@ -3,31 +3,28 @@ from .models import Startup, Contact, StartupSubmit
 from rest_framework.decorators import api_view, APIView
 from rest_framework.response import Response
 from rest_framework import status, viewsets
-from .serializers import StartupSerializer, ContactSerializer
+from .serializers import StartupSerializer, ContactSerializer, StartupSubmitSerializer
 
 
 
 def index(request):
     return HttpResponse("index page")
 
-@api_view(['GET'])
-def startups(request):
-    if request.method == 'GET':
-        startup = Startup.objects.all()
-        serializer = StartupSerializer(startup, many=True,context = {startup:startup})
-        return Response(serializer.data)
-    
-def startupsDetail(request, pk):
-    startup = Startup.objects.get(id=pk)
-    serializer = StartupSerializer(startup, many=False)
-    return Response(serializer.data, pk = startup.id)
 
-    
+class StartupViewSet(viewsets.ModelViewSet):
+    queryset = Startup.objects.all()
+    serializer_class = StartupSerializer
+    http_method_names = ['get']
+
+
+
 class ContactViewSet(viewsets.ModelViewSet):
     queryset = Contact.objects.all()
     serializer_class = ContactSerializer
+    http_method_names = ['post']
 
 
 class StartupSubmitViewSet(viewsets.ModelViewSet):
     queryset = StartupSubmit.objects.all()
-    serializer_class = StartupSerializer
+    serializer_class = StartupSubmitSerializer
+    http_method_names = ['post']
