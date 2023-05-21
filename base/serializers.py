6272e57da1a28e346fb2c1c9ff5761686090999f
farazzkamali <farazzkamali80@gmail.com>
2023-05-21 09:32:10 -0400

    fix: Change Serializer method

--- base/serializers.py
+++ base/serializers.py
@@ -1,19 +1,21 @@
-from .models import Startup, Contact
+from .models import Startup, Contact, StartupSubmit
 from rest_framework import serializers 
 
 
 class StartupSerializer(serializers.ModelSerializer):
     class Meta:
         model = Startup
-        fields = '__all__'
+        fields = ('id', 'name', 'members', 'fund', 'description', 'logo')
 
-class ContactSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Contact
-        fields = '__all__'
+class ContactSerializer(serializers.Serializer):
+    name = serializers.CharField()
+    email = serializers.EmailField()
+    phone = serializers.CharField(required=True)
+    message = serializers.CharField()
 
-
-class StartupSubmitSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Startup
-        fields = '__all__'
+class StartupSubmitSerializer(serializers.Serializer):
+    name = serializers.CharField()
+    members_count = serializers.IntegerField()
+    email = serializers.EmailField()
+    phone = serializers.CharField(required=True)
+    pitch = serializers.FileField(required=True)
