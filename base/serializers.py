d6a295ff2a63ebf7fa4ec8d278d44dea394c05b2
farazzkamali <farazzkamali80@gmail.com>
2023-05-23 11:03:23 -0400

    fix: Rewrite to fix create method & validations

--- base/serializers.py
+++ base/serializers.py
@@ -1,4 +1,4 @@
-from .models import Startup, Event
+from .models import Startup, Event, Contact, StartupSubmit
 from rest_framework import serializers 
 
 
@@ -7,18 +7,41 @@ class StartupSerializer(serializers.ModelSerializer):
         model = Startup
         fields = ('id', 'name', 'members', 'fund', 'description', 'logo')
 
-class ContactSerializer(serializers.Serializer):
-    name = serializers.CharField()
-    email = serializers.EmailField()
-    phone = serializers.CharField(required=True)
-    message = serializers.CharField()
 
-class StartupSubmitSerializer(serializers.Serializer):
-    name = serializers.CharField()
-    members_count = serializers.IntegerField()
-    email = serializers.EmailField()
-    phone = serializers.CharField(required=True)
-    pitch = serializers.FileField(required=True)
+class ContactSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Contact
+        fields = ['id', 'name', 'email', 'message', 'phone', 'created_at']
+        read_only_fields = ['id', 'created_at']
+
+    def create(self, validated_data):
+        validated_data['phone'] = self.initial_data.get('phone')
+        
+        if not validated_data['phone']:
+            raise serializers.ValidationError("Phone is required.")
+
+        return super().create(validated_data)
+    
+    
+class StartupSubmitSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = StartupSubmit
+        fields = ['id', 'name', 'members_count', 'email', 'phone', 'pitch']
+        read_only_fields = ['id']
+
+    def create(self, validated_data):
+        validated_data['phone'] = self.initial_data.get('phone')
+        validated_data['pitch'] = self.initial_data.get('pitch')
+
+        if not validated_data['phone']:
+            raise serializers.ValidationError("Phone is required.")
+
+        if not validated_data['pitch']:
+            raise serializers.ValidationError("Pitch is required.")
+
+        return super().create(validated_data)
+
+
 
 class EventSerializer(serializers.ModelSerializer):
     class Meta:
