111bfc9a358b138e956e352fab5a2ffa212e4829
farazzkamali <farazzkamali80@gmail.com>
2023-05-23 03:05:46 -0400

    feat: register event

--- base/admin.py
+++ base/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Startup, Contact, StartupSubmit
+from .models import Startup, Contact, StartupSubmit, Event
 
 admin.site.register(Startup)
 admin.site.register(Contact)
 admin.site.register(StartupSubmit)
+admin.site.register(Event)
