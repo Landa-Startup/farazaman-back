30e2d2517a08316be50bc6ae871f46523ab26633
farazzkamali <farazzkamali80@gmail.com>
2023-06-06 03:58:00 -0400

    feat : register Hire model

--- base/admin.py
+++ base/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Startup, Contact, StartupSubmit, Event
+from .models import Startup, Contact, StartupSubmit, Event, Hire
 
 admin.site.register(Startup)
 admin.site.register(Contact)
 admin.site.register(StartupSubmit)
 admin.site.register(Event)
+admin.site.register(Hire)
