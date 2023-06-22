1ee2f5021b8c71448c0e58212a42c082d5631529
Meraj Bighamian <merajbighamian@gmail.com>
2023-06-22 14:24:28 +0430

    fix logo model field

--- base/models.py
+++ base/models.py
@@ -3,10 +3,10 @@ from django.db import models
 
 class Startup (models.Model):
     name = models.CharField(max_length=255)
-    members = models.TextField()
+    members = models.IntegerField()
     fund = models.CharField(blank=True, null=True, max_length=1000)
     description = models.TextField(blank=True, null=True)
-    logo = models.CharField(max_length=1255)
+    logo = models.ImageField(upload_to='base/logos')
 
     def __str__(self):
         return self.name
